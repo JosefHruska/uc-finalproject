3ce0b3d9a103975024c5637f110af192856fbc1b
Josef Hruška <Josef Hruka>
2016-01-29 20:00:48 +0100

    testik

---
+++ app/src/main/java/com/example/android/studyproject/MainActivityFragment.java
@@ -0,0 +1,145 @@
+package com.example.android.studyproject;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private ArrayAdapter<String> defaultAdapter;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Add this line in order for this fragment to handle menu events.
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        String[] data = {
+                "Two", "Top", "Policemen", "Met", "On", "The", "Line",
+                "Two", "Top", "Policemen", "Met", "On", "The", "Line",
+                "Two", "Top", "Policemen", "Met", "On", "The", "Line",
+                "Two", "Top", "Policemen", "Met", "On", "The", "Line"
+        };
+
+        List<String> placeholderData = new ArrayList<String>(Arrays.asList(data));
+
+        defaultAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.basic_layout,
+                R.id.basic_view,
+                placeholderData
+                );
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        GridView gridView = (GridView) rootView.findViewById(R.id.grid_view);
+        gridView.setAdapter(defaultAdapter);
+        FetchMovieDatabase movieTask = new FetchMovieDatabase();
+        movieTask.execute();
+        return rootView;
+    }
+
+    public class FetchMovieDatabase extends AsyncTask<Void, Void, Void> {
+
+        private final String LOG_TAG =  FetchMovieDatabase.class.getSimpleName();
+
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String moviesJsonStr = null;
+
+            try {
+            String baseUrl = "https://api.themoviedb.org/3/movie/550?api_key=";
+            String api = "433e57f96e89ea06704dd7bca2f88048";
+            URL url = new URL(baseUrl.concat(api));
+
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                moviesJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                Log.d(LOG_TAG,"output" + moviesJsonStr);
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
+
+    }
+}
